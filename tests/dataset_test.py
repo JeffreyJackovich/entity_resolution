3edef299ada79306c7622258a852bb05ae7fcd6c
jeffrey jackovich <jeffrey.jackovich@gmail.com>
2021-08-03 14:18:49 -0400

    Add DatasetReader unit test feature

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -2,6 +2,8 @@
 from pytest import fixture
 from entity_resolution.dataset import Dataset
 from os import path
+from entity_resolution.dataset import DatasetReader
+
 
 ABS_PATH = path.abspath('..')
 EXTERNAL_DATA_DIR = '/data/external/'
@@ -16,3 +18,18 @@ def test_source_url(my_dataset):
 def test_source_filename(my_dataset):
     assert my_dataset.contributions_zip_file == 'Illinois-campaign-contributions.txt.zip'
 
+
+
+@fixture
+def my_dataset_reader():
+    return DatasetReader(PATH=ABS_PATH+EXTERNAL_DATA_DIR)
+
+def test_dataset_len(my_dataset_reader):
+    """
+
+    :return:
+    """
+    expected = 1762975
+    actual = my_dataset_reader.__len__()
+    message = "Expected return value to be: {expected}"
+    assert actual == expected, message
